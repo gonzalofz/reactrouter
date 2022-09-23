fcc08fb53fb566c96bf440f5604f0a1ffb402767
gonzalofz <102331798+gonzalofz@users.noreply.github.com>
2022-09-22 22:09:28 -0300

    first commit

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <>
+      <Container>
+        <center>
+          <h1>Bienvenido a mi primera pagina con React Router</h1>
+          <p>Gonzalo fuentes 2022 el crack!</p>
+          <img
+            src="https://plantillasdememes.com/img/plantillas/joseph-ducreux-es-un-crack01626064667.jpg"
+            alt="di que si"
+            height="400"
+          />
+        </center>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
